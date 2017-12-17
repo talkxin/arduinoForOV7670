0906c68af555b9037314a52763eb352ca460ffe6
liuxin <liuxin01@sdc.icbc.com.cn>
2017-12-17 23:14:56 +0800

    树莓派内核编译脚本

---
+++ rpi-build.sh
@@ -0,0 +1,42 @@
+TOOLCHAIN=$(pwd)/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian/bin/arm-linux-gnueabihf
+rm -rf kernel-qemu
+mkdir kernel-qemu
+cd kernel-qemu
+wget https://github.com/polaco1782/raspberry-qemu/blob/master/linux-arm.patch
+git clone git://github.com/raspberrypi/linux.git --branch rpi-4.9.y --single-branch
+git clone git://github.com/raspberrypi/tools.git
+cd linux
+patch -p1 < ../linux-arm.patch
+
+make ARCH=arm versatile_defconfig
+cat >> .config << EOF
+CONFIG_CROSS_COMPILE="$TOOLCHAIN"
+CONFIG_CPU_V6=y
+CONFIG_ARM_ERRATA_411920=y
+CONFIG_ARM_ERRATA_364296=y
+CONFIG_AEABI=y
+CONFIG_OABI_COMPAT=y
+CONFIG_PCI=y
+CONFIG_SCSI=y
+CONFIG_SCSI_SYM53C8XX_2=y
+CONFIG_BLK_DEV_SD=y
+CONFIG_BLK_DEV_SR=y
+CONFIG_DEVTMPFS=y
+CONFIG_DEVTMPFS_MOUNT=y
+CONFIG_TMPFS=y
+CONFIG_INPUT_EVDEV=y
+CONFIG_EXT3_FS=y
+CONFIG_EXT4_FS=y
+CONFIG_VFAT_FS=y
+CONFIG_NLS_CODEPAGE_437=y
+CONFIG_NLS_ISO8859_1=y
+CONFIG_FONT_8x16=y
+CONFIG_LOGO=y
+CONFIG_VFP=y
+CONFIG_CGROUPS=y
+EOF
+
+make -j 8 -k ARCH=arm CROSS_COMPILE=${TOOLCHAIN}- menuconfig
+make -j 8 -k ARCH=arm CROSS_COMPILE=${TOOLCHAIN}-
+cd ..
+cp linux/arch/arm/boot/zImage kernel-qemu
